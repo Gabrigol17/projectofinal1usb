1ed7a33101d7c6fe6c607455779ecc8084d0421b
Gabrigol17 <gabrielangelarrieta3@gmail.com>
2025-11-16 15:11:16 -0500

    .

--- proyectofinaljava/src/main/java/com/example/proyectofinaljava/App.java
+++ proyectofinaljava/src/main/java/com/example/proyectofinaljava/App.java
@@ -2,6 +2,11 @@ package com.example.proyectofinaljava;
 
 public class App {
     public static void main(String[] args) {
-        
+          
+    }
+    int mapa[][]=new int[15][15];
+    
+    public static void left_move(int m[][],int i,int j){
+
     }
 }
