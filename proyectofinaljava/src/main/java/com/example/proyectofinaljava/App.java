29a6975d9ca9a8a1315edd599acdaf3c2fa53291
Bladimir <168496700+blacho12@users.noreply.github.com>
2025-11-16 14:00:50 -0500

    Sebas Update

--- proyectofinaljava/src/main/java/com/example/proyectofinaljava/App.java
+++ proyectofinaljava/src/main/java/com/example/proyectofinaljava/App.java
@@ -1,7 +1,134 @@
 package com.example.proyectofinaljava;
 
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) {
-        
+        int posj_y, posj_x, posf_y, posf_x, posi_x, posi_y, i, j;
+
+        int mapa1[][] = {
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+        };
+        int mapa2[][] = {
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+        };
+
+
+
+        showMap(mapa1);
     }
+
+
+    public static void showMap(int M[][]){
+        int i,j;
+        for(i=0; i<15; i++){
+            for(j=0; j<15; j++){
+                System.out.print(M[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+     public static void move_left(int posj_y, int posj_x, int posf_y, int posf_x, int map[][]) {
+
+        if (map[posj_x-1][posj_y] != 1) {
+            JOptionPane.showMessageDialog(null, "el jugador se movio arriba");
+            map[posj_x][posj_y] = 0;
+            map[posj_x-1][posj_y] = 9;
+            posj_y -= 1;
+            
+            win(posj_y, posj_x, posf_y, posf_x, map);
+        
+        } else {
+            JOptionPane.showMessageDialog(null, "no se puede es un muro intente otra vez");
+        }
+
+    }
+
+    public static void move_rigth(int posj_y, int posj_x, int posf_y, int posf_x, int map[][]) {
+
+        if (map[posj_x+1][posj_y] != 1) {
+            JOptionPane.showMessageDialog(null, "el jugador se movio arriba");
+            map[posj_x][posj_y] = 0;
+            map[posj_x+1][posj_y] = 9;
+            posj_y += 1;
+            
+            win(posj_y, posj_x, posf_y, posf_x, map);
+        
+        } else {
+            JOptionPane.showMessageDialog(null, "no se puede es un muro intente otra vez");
+        }
+
+    }
+
+
+    public static void move_up(int posj_y, int posj_x, int posf_y, int posf_x, int map[][]) {
+
+        if (map[posj_x][posj_y + 1] != 1) {
+            JOptionPane.showMessageDialog(null, "el jugador se movio arriba");
+            map[posj_x][posj_y] = 0;
+            map[posj_x][posj_y + 1] = 9;
+            posj_y += 1;
+            
+            win(posj_y, posj_x, posf_y, posf_x, map);
+        
+        } else {
+            JOptionPane.showMessageDialog(null, "no se puede es un muro intente otra vez");
+        }
+
+    }
+
+    public static void move_down(int posj_y, int posj_x, int posf_y, int posf_x, int map[][]) {
+
+        if (map[posj_x][posj_y - 1] != 1) {
+            JOptionPane.showMessageDialog(null, "el jugador se movio arriba");
+            map[posj_x][posj_y] = 0;
+            map[posj_x][posj_y - 1] = 9;
+            posj_y -= 1;
+            
+            win(posj_y, posj_x, posf_y, posf_x, map);
+        
+        } else {
+            JOptionPane.showMessageDialog(null, "no se puede es un muro intente otra vez");
+        }
+
+    }
+
+
+    public static void win(int posj_y, int posj_x, int posf_y, int posf_x, int map[][]) {
+        if (posj_y == posf_y && posj_x == posf_x) {
+            JOptionPane.showMessageDialog(null, "Ganaste");
+        }
+    }
+
+
 }
